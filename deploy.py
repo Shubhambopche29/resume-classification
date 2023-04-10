e0e0deec9566bf0e570dd6964a031f98fa55f485
SHUBHAM BOPCHE <116349421+Shubhambopche29@users.noreply.github.com>
2023-04-11 02:27:26 +0530

    Add files via upload

---
+++ deploy.py
@@ -0,0 +1,153 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+import pickle
+
+
+model = pickle.load(open('C:/Users/shubh/Downloads/resume_dep/Resume_nlp.sav','rb'))
+model1 = pickle.load(open('C:/Users/shubh/Downloads/resume_dep/vector.pkl','rb'))
+
+
+def extract_skills(resume_text):
+
+    nlp_text = nlp(resume_text)
+    noun_chunks = nlp_text.noun_chunks
+
+    # removing stop words and implementing word tokenization
+    tokens = [token.text for token in nlp_text if not token.is_stop]
+            
+    # reading the csv file
+    data = pd.read_csv("C:/Users/shubh/Downloads/resume_dep/resumes.csv")
+
+            
+    # extract values
+    skills = list(data.columns.values)
+            
+    skillset = []
+            
+    # check for one-grams (example: python)
+    for token in tokens:
+        if token.lower() in skills:
+            skillset.append(token)
+            
+    # check for bi-grams and tri-grams (example: machine learning)
+    for token in noun_chunks:
+        token = token.text.lower().strip()
+        if token in skills:
+            skillset.append(token)
+            
+    return [i.capitalize() for i in set([i.lower() for i in skillset])]
+
+# Function to extract text from resume
+def getText(filename):
+      
+    # Create empty string 
+    fullText = ''
+    if filename.type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+        doc = docx2txt.process(filename)
+        
+        for para in doc:
+            fullText = fullText + para
+            
+           
+    else:  
+        with pdfplumber.open(filename) as pdf_file:
+            pdoc = PyPDF2.PdfFileReader(filename)
+            number_of_pages = pdoc.getNumPages()
+            page = pdoc.pages[0]
+            page_content = page.extractText()
+             
+        for paragraph in page_content:
+            fullText =  fullText + paragraph
+         
+    return (fullText)
+
+def display(doc_file):
+    resume = []
+    if doc_file.type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+        resume.append(docx2txt.process(doc_file))
+
+    else:
+        with pdfplumber.open(doc_file) as pdf:
+            pages=pdf.pages[0]
+            resume.append(pages.extract_text())
+            
+    return resume
+
+def preprocess(sentence):
+    sentence=str(sentence)
+    sentence = sentence.lower()
+    sentence=sentence.replace('{html}',"") 
+    cleanr = re.compile('<.*?>')
+    cleantext = re.sub(cleanr, '', sentence)
+    rem_url=re.sub(r'http\S+', '',cleantext)
+    rem_num = re.sub('[0-9]+', '', rem_url)
+    tokenizer = RegexpTokenizer(r'\w+')
+    tokens = tokenizer.tokenize(rem_num)  
+    filtered_words = [w for w in tokens if len(w) > 2 if not w in stopwords.words('english')]
+    lemmatizer = WordNetLemmatizer()
+    lemma_words=[lemmatizer.lemmatize(w) for w in filtered_words]
+    return " ".join(lemma_words)  
+
+
+# Function to extract experience details
+def expDetails(Text):
+    global sent
+   
+    Text = Text.split()
+   
+    for i in range(len(Text)-2):
+        Text[i].lower()
+        
+        if Text[i] ==  'years':
+            sent =  Text[i-2] + ' ' + Text[i-1] +' ' + Text[i] +' '+ Text[i+1] +' ' + Text[i+2]
+            l = re.findall(r'\d*\.?\d+',sent)
+            for i in l:
+                a = float(i)
+            return(a)
+            return (sent)
+
+target = {0:'Peoplesoft',1:'SQL Developer',2:'React JS Developer',3:'Workday'}
+
+def main():
+    html_temp = """
+    <div style ="background-color:transparent;padding:13px">
+    <h1 style ="color:#576CBC;text-align:center;"> RESUME CLASSIFICATION </h1>
+    </div>
+    """
+    st.markdown(html_temp, unsafe_allow_html = True)
+
+    file_type=pd.DataFrame([], columns=['Uploaded File', 'Experience', 'Skills', 'Predicted Profile'])
+    filename = []
+    predicted = []
+    experience = []
+    skills = []
+    
+    upload_file = st.file_uploader('Hey,Upload Your Resumes ',
+                                type= ['docx','pdf'],accept_multiple_files=True)
+
+    for doc_file in upload_file:
+            if doc_file is not None:
+                filename.append(doc_file.name)
+                cleaned=preprocess(display(doc_file))
+                prediction = model.predict(model1.transform([cleaned]))[0]
+                predicted.append(target.get(prediction))
+                extText = getText(doc_file)
+                exp = expDetails(extText)
+                experience.append(exp)
+                skills.append(extract_skills(extText))
+    if len(predicted) > 0:
+        file_type['Uploaded File'] = filename
+        file_type['Experience'] = experience
+        file_type['Skills'] = skills
+        file_type['Predicted Profile'] = predicted
+        # file_type
+        # Custom formatting
+        st.table(file_type.style.format({'Experience': '{:.1f}'}))
+
+    st.write(f'*Note: Classifies only for Workday, Peoplesoft, React JS and SQL Developer Resumes')
+    st.subheader("About") 
+    st.info("This project is a part of AiVariant Internship")
+   
+if __name__ == '__main__':
+     main()
